51f6498d19b09817cb1a91cd75b7317e39d0bc64
Deepak Sharma <deepak.sharma.it.1992@gmail.com>
2019-08-30 15:12:19 +0530

    First commit for Tensorflow js web app submission

---
+++ static/js/main.js
@@ -0,0 +1,36 @@
+let net;
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+
+  // Make a prediction through the model on our image.
+  const imgEl = document.getElementById('img');
+  const result = await net.classify(imgEl);
+  console.log(result);
+  document.getElementById("predicted_text").innerHTML = result[0].className;
+
+}
+
+function readURL(input) {
+          if (input.files && input.files[0]) {
+              var reader = new FileReader();
+
+              reader.onload = function (e) {
+                  $('#img')
+                      .attr('src', e.target.result);
+              };
+
+              console.log(input.files[0])
+              reader.readAsDataURL(input.files[0]);
+          }
+}
+
+$("#file-picker").change(function () {
+        readURL(this);
+        app();
+ });
+
